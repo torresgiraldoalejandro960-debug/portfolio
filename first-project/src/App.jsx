cba27238eea84c89931cd94411299ebb0a192637
Alejandro Torres <torresgiraldoalejandro960@gmail.com>
2026-07-31 19:32:36 -0500

    Project start

---
+++ first-project/src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from "./Header/Header"
+
+export const App = () => {
+  return(
+    // fragment: etiqueta vacia 
+    <>
+    <Header/>
+    <h1> Bienvenido </h1>
+    <h2> a mi mundo </h2>
+    </>
+  )
+}
---
+++ first-project/src/Header/Header.jsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+  return(
+    <header>
+      <nav>
+        <ul>
+           <li><a href="">Inicio</a></li>
+           <li><a href="">Acerca de</a></li>
+           <li><a href="">Contactos</a></li>
+           <li><a href="">FQA</a></li>
+        </ul>
+      </nav>
+    </header>
+
+  )
+}
