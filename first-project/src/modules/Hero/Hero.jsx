0b2a4154fc36959c59aa0f814def430bfa20c729
Alejandro Torres <torresgiraldoalejandro960@gmail.com>
2026-09-11 16:19:21 -0500

    agrego foto formal para la hoja de vida

--- first-project/src/modules/Hero/Hero.jsx
+++ first-project/src/modules/Hero/Hero.jsx
@@ -1,8 +1,10 @@
 import styles from './Hero.module.css';
+import profileImage from '../../assets/profile.jpg';
 
 const Hero = ({ nombre, titulo, descripcion }) => {
   return (
     <section className={styles.heroContainer}>
+      <img src={profileImage} alt={nombre} className={styles.profileImage} />
       <h1>{nombre}</h1>
       <p className={styles.heroSubtitle}>{titulo}</p>
       <p className={styles.heroDescription}>{descripcion}</p>
